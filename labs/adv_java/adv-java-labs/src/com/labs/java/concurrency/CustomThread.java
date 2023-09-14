3f62731c602ae90673bf6995cd3da0d798cf22b3
vigneshMahenthiran <106167038+vigneshMahenthiran@users.noreply.github.com>
2023-09-14 10:56:26 +0530

    completed until assignment 9
    completed bankingapp-with-impexp-thread

--- labs/adv_java/adv-java-labs/src/com/labs/java/concurrency/CustomThread.java
+++ labs/adv_java/adv-java-labs/src/com/labs/java/concurrency/CustomThread.java
@@ -1,9 +1,5 @@
 package com.labs.java.concurrency;
 
 public class CustomThread extends Thread{
-    public void run(){
-        System.out.println("entered into "+Thread.currentThread().getName());
-        System.out.println("*********");
-        System.out.println("exited from "+Thread.currentThread().getName());
-    }
+
 }
